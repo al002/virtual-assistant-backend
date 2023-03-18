eebaca004868b611a23b1b5b6fa080557163d0cd
al002 <al002@disroot.org>
2023-03-18 16:59:04 +0800

    add bash operation

---
+++ virtual_assistant_chain/chains/bash/base.py
@@ -0,0 +1,31 @@
+from langchain.chains import LLMBashChain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+
+from virtual_assistant_chain.chains.bash.prompt import CREATE_FOLDER_TEMPLATE, CREATE_NEXT_APP_TEMPLATE, CREATE_POETRY_PYTHON_PROJECT
+
+class BashChain:
+    chain: LLMBashChain
+
+    def __init__(self) -> None:
+        llm = OpenAI(temperature=0.5)
+        self.chain = LLMBashChain(llm=llm, verbose=True)
+
+    def create_folder(self, folder_name: str, dest: str = "current directory"):
+        input = PromptTemplate.from_template(CREATE_FOLDER_TEMPLATE).format(name=folder_name, dest=dest)
+        self.run(input)
+
+    def create_next_app(self, app_name: str):
+        input = PromptTemplate.from_template(CREATE_NEXT_APP_TEMPLATE).format(app_name=app_name)
+        self.run(input)
+
+    def create_python_project(self, app_name: str):
+        input = PromptTemplate.from_template(CREATE_POETRY_PYTHON_PROJECT).format(app_name=app_name)
+        self.run(input)
+
+    def run(self, input: str):
+        self.chain.run(input)
+
+if __name__ == "__main__":
+    bash = BashChain()
+    bash.create_python_project(app_name="test2")
---
+++ virtual_assistant_chain/chains/bash/prompt.py
@@ -0,0 +1,5 @@
+CREATE_FOLDER_TEMPLATE = "Create a folder named {name} under {dest}"
+
+CREATE_NEXT_APP_TEMPLATE = "Create NextJS app named {app_name}, it should use typescript"
+
+CREATE_POETRY_PYTHON_PROJECT = "Using poetry to create a python project named {app_name}"
